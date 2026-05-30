e83450dadba955f1244e17fb4d4941a70707ee3b
maksym06-es <maksym8901@gmail.com>
2026-05-30 16:50:22 +0200

    fix cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const orderRoutes = require('./routes/orders');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
